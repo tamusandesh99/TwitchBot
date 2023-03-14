58445c67fd230ad287748c45ab08d279eb611c20
tamusandesh99 <tamusandesh99@gmail.com>
2023-03-14 11:45:02 -0400

    moved tesseract and mss into seperate file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from twitchio.ext import commands
 import config
 import mss
@@ -5,71 +7,26 @@ import mss.tools
 import time
 from PIL import Image
 from pytesseract import pytesseract
-total_death = 51
-
-bot = commands.Bot(
-    token=config.TMI_TOKEN,
-    client_id=config.CLIENT_ID,
-    nick=config.BOT_NICK,
-    prefix=config.BOT_PREFIX,
-    initial_channels=[config.CHANNEL],
-)
-
-
-@bot.event()
-async def event_message(ctx):
-    print(ctx.author)
-    print(ctx.content)
-    await bot.handle_commands(ctx)
-
-
-@bot.event()
-async def sendRunCount(run: int):
-    ws = bot._ws
-    await ws.send_privsgm
-
-
-@bot.command(name='challenges')
-async def test_command(ctx):
-    await ctx.send("Brett is the god")
-
-
-@bot.command(name='fist')
-async def test_command(ctx):
-    await ctx.send("Fisted so far: Radagon, Maliketh, Radahn, Margit, Morgott")
-
-
-@bot.command(name='streamMind')
-async def test_command(ctx):
-    await ctx.send("Yep. Its Golan and she VIP too")
-
-
-@bot.command(name='whoisW')
-async def test_command(ctx):
-    await ctx.send("Its Jake. W Jake")
-
-
-@bot.command(name='babyname')
-async def test_command(ctx):
-    await ctx.send("Fire Giant")
 
 
 def main():
-    global total_death
-    myconfig = r"--psm 3 --oem 3"
+    """ Start of the screen capture """
+    total_run = get_count()  # Getting the total run stored in json file
+    myconfig = r"--psm 3 --oem 3"  # initializing the configuration for tesseract
     with mss.mss() as sct:
         while True:
-            monitor_number = 1
+            monitor_number = 2  # screenshot pointing to 2nd monitor
             mon = sct.monitors[monitor_number]
 
-            # The screen part to capture
+            # The screen part to capture. Captures the center of screen
             monitor_died = {
-                "top": mon["top"] + 410,  # 100px from the top
-                "left": mon["left"] + 550,  # 100px from the left
+                "top": mon["top"] + 410,  # 410px from the top
+                "left": mon["left"] + 550,  # 550px from the left
                 "width": 450,
                 "height": 160,
                 "mon": monitor_number,
             }
+            #  Screen to capture. Captures the center bottom of screen
             monitor_blade = {
                 "top": mon["top"] + 710,  # 100px from the top
                 "left": mon["left"] + 550,  # 100px from the left
@@ -77,33 +34,66 @@ def main():
                 "height": 110,
                 "mon": monitor_number,
             }
+
+            #  describing path and file name for screenshot
             output_died = "./screenshots/died.png".format(**monitor_died)
             output_blade = "./screenshots/blade.png".format(**monitor_died)
 
-            # Grab the data
+            # Grabs the screenshot of two positions in screen
             sct_img_died = sct.grab(monitor_died)
             sct_img_blade = sct.grab(monitor_blade)
 
-            # Save to the picture file
+            # Save to the picture file (png)
             mss.tools.to_png(sct_img_died.rgb, sct_img_died.size, output=output_died)
             mss.tools.to_png(sct_img_blade.rgb, sct_img_blade.size, output=output_blade)
 
+            #  sending the screenshot to tesseract to convert it to text
             text_died = pytesseract.image_to_string('./screenshots/died.png', config=myconfig)
             text_blade = pytesseract.image_to_string('./screenshots/blade.png', config=myconfig)
 
-            print(text_died)
-            print(text_blade)
-            if 'Malenia' in text_blade or 'Blade' in text_blade or 'Miquella' in text_blade:
+            # Original to match with screenshot texts
+            dialogues = ['Malenia', 'Blade', 'Miquella',
+                         'Flesh', 'Let', 'Your', 'be', 'consumed',
+                         'By', 'scarlet', 'rot']
+            deathMark = ['YOU', 'DIED', 'OUD', 'OU', 'IED', 'YO']
+
+            # print(text_died)
+            # print(text_blade)
+            #  Compares if the texts in screenshot matches with default condition text
+            if any([x in text_blade for x in dialogues]):
                 print("Blade")
-                total_death += 1
-                time.sleep(5)
-            if 'YOU' in text_died or 'DIED' in text_died:
+                total_run += 1
+                update_count(total_run)  # update the value in json if conditions met
+                time.sleep(7)
+            if any([x in text_died for x in deathMark]):
                 print('Died')
-                total_death += 1
+                total_run += 1
+                update_count(total_run)  # update the value in json if conditions met
                 time.sleep(5)
-            print(total_death)
+            print(total_run)
+
+
+def get_count():
+    """ Reads the count from the JSON file and returns it """
+    with open(config.JSON_FILE) as json_file:
+        data = json.load(json_file)
+        return data['total_run']
+
+
+def update_count(count):
+    """ Updates the JSON file with count given """
+    data = None
+
+    with open(config.JSON_FILE) as json_file:
+        data = json.load(json_file)
+
+    if data is not None:
+        data['total_run'] = count
+
+    with open(config.JSON_FILE, 'w') as json_file:
+        json.dump(data, json_file, sort_keys=True, indent=4)
 
 
 if __name__ == '__main__':
-    # main()
-    bot.run()
+    main()
+
